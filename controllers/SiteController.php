1be8ce4800fbe821df08c1632fd42937436a8f5e
Nguidjo <50013726+Nguidjo@users.noreply.github.com>
2021-03-06 16:14:42 +0100

    Update SiteController.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * User: TheCodeholic
- * Date: 7/8/2020
- * Time: 8:43 AM
- */
 
 namespace app\controllers;
 
@@ -32,7 +27,7 @@ class SiteController extends Controller
     public function home()
     {
         return $this->render('home', [
-            'name' => 'TheCodeholic'
+            'name' => 'Muuska'
         ]);
     }
 
